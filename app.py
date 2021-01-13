3538536d27f9ba02ef176b26d4a512b87827cca0
Geoff Groenendale <ggroenendale@wsu.edu>
2021-01-13 10:59:58 -0800

    Updated main app

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{basedir}/dev.db"
 
+
 @app.route("/api")
 def api_home():
     return "API is working please add routes"
+
 
 if __name__ == "__main__":
     app.run(host="192.168.1.2", port=80)
